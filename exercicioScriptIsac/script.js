05d76cb41fda0d07c05f3b1b000f2de01b49a106
FreitasIsac <166455554+FreitasIsac@users.noreply.github.com>
2025-03-31 19:30:27 -0300

    Add files via upload

---
+++ exercicioScriptIsac/script.js
@@ -0,0 +1,9 @@
+var numero = Number(prompt("Digite um numero. para saber se é positivo ou negativo"))
+
+if(numero >0) {
+    alert("Positivo")
+} if(numero ==0) {
+    alert("Igual a Zero")
+} else {
+    alert("Negativo")
+}
